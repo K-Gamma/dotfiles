70dcc82fc330c63bb07bdbe73e0b62e9216e671c
K-Ganma <katou.gannma-@hotmail.co.jp>
2017-08-24 02:51:18 +0900

    Try one-liner

--- install.sh
+++ install.sh
@@ -1,123 +1,89 @@
 #!/bin/bash -u
-# @(#) This script make symbolic link of dotfiles.
+# @(#) This script build an environment.
 
-if [ $# -gt 1 ]; then
+if [ $# -gt 0 ]; then
     echo "Error: wrong number of arguments" 1>&2
     exit 1
 fi
 
-# working directory
-WD=$(cd "${0%/*}" && pwd)
+has() {
+    which $1 > /dev/null 2>&1
+    return $?
+}
+
+readonly DOTPATH=~/.dotfiles
+readonly GITHUB_URL=https://github.com/K-Ganma/dotfiles.git
+
+# if git is installed, use git.
+if has "git"; then
+    git clone --recursive "$GITHUB_URL" "$DOTPATH"
+    # if git isn't installed, use curl or wget.
+elif has "curl" || has "wget"; then
+    readonly TAR_URL=https://github.com/K-Ganma/dotfiles/archive/master.tar.gz
+    if has "curl"; then
+        curl -fL "$TAR_URL" 
+    elif has "wget"; then
+        wget -O - "$TAR_URL"
+    fi | tar xzvf -
+    mv -f dotfiles-master "$DOTPATH"
+elif
+    echo "Error : 'curl' or 'wget' required." 1>&2
+fi
+
+cd ~/.dotfiles
+if [$? -ne 0]; then
+    echo "Error : $DOTPATH can't found." 1>&2
+fi
 
 # delimiter
 IFS_ORG=$IFS
 IFS=$'\n'
 
 # original file
-ODIRS=($(find ${WD}/ -mindepth 1 -type d | grep -v ".git"))
-OFILES=($(find ${WD}/ -type f | grep -E -v ".git/|README.md|${0##*/}"))
+ODIRS=($(find ${DOTPATH}/ -mindepth 1 -type d | grep -v ".git"))
+OFILES=($(find ${DOTPATH}/ -type f | grep -E -v ".git/|README.md|${0##*/}"))
 
 IFS=$IFS_ORG
 
-# 関数の定義
-# Install {{{
-install() {
-    local odir dir
-    # Make dir
-    echo $'\n# Make dir'
-    for odir in "${ODIRS[@]}"; do
-        dir=${odir//\/dotfiles/}
-        if [ ! -e "${dir}" ]; then
-            echo "mkdir $dir"
-            mkdir "${dir}"
-        elif [ ! -d "${dir}" ]; then
-            echo "Cannot create : ${dir}"
-        else
-            echo "Already exist : ${dir}"
-        fi
-    done
-
-    local ofile file
-    # Make symbolic link
-    echo $'\n# Make symbolic link'
-    for ofile in "${OFILES[@]}"; do
-        file=${ofile//\/dotfiles/}
-        if [ ! -e "${file}" ]; then
-            echo "${ofile} --> ${file}"
-            ln -s ${ofile} ${file}
-        elif [ ! -f "${file}" ]; then
-            echo "Cannot create : ${file}"
-        else
-            echo "Already exist : ${file}"
-        fi
-    done
-
-
-    # ---------------- other config ---------------- #
-
-    # Make symbolic link of .zshenv into ~/
-    if [ -f ~/.zsh/.zshenv -a ! -f ~/.zshenv ]; then
-        echo "~/.zsh/.zshenv --> ~/.zshenv"
-        ln -sf ~/.zsh/.zshenv ~/.zshenv
-    fi
-
-    echo $'\n\n|++++++++++++++++ DOTFILES IS INSTALLED ! ++++++++++++++++|\n\n'
-
-}
-# }}}
-
-# Uninstall {{{
-uninstall () {
-    local ofile file
-    # Remove symbolic link
-    echo $'\n# Remove symbolic link'
-    for ofile in "${OFILES[@]}"; do
-        file=${ofile//\/dotfiles/}
-        if [ -e "${file}" ]; then
-            echo "rm -rf ${file}"
-            rm -rf ${file}
-        fi
-    done
-
-    # Remove dir
-    local odir dir
-    echo $'\n# Remove dir'
-    for odir in "${ODIRS[@]}"; do
-        dir=${odir//\/dotfiles/}
-        if [ -e "${dir}" ]; then
-            echo "rm -rf ${dir}"
-            rm -rf ${dir}
-        fi
-    done
-
-
-    # ---------------- other config ---------------- #
-
-    # Remove symbolic link of .zshenv into ~/
-    if [ -L ~/.zshenv ]; then
-        echo "rm -rf ~/.zshenv"
-        rm -rf ~/.zshenv
-    fi
-
-    echo "\n\n|++++++++++++++++ DOTFILES IS DELETED ! ++++++++++++++++|\n\n"
-
-}
-# }}}
-
-if [ $# -eq 1 ]; then
-    if [ $1 = "clean" ]; then
-        read -p 'Are you sure to deleted dotfiles and some plugin ? [y/N] > ' ANSWER
-        case $ANSWER in
-            "Y" | "y" | "yes" | "Yes" | "YES" ) uninstall;;
-            * ) exit 1;;
-        esac
+# Make dir
+echo $'\n# Make dir'
+for odir in "${ODIRS[@]}"; do
+    dir=${odir//\/.dotfiles/}
+    if [ ! -e "${dir}" ]; then
+        echo "mkdir $dir"
+        mkdir "${dir}"
+    elif [ ! -d "${dir}" ]; then
+        echo "Cannot create : ${dir}"
     else
-        echo "Error: wrong arguments" 1>&2
-        exit 1
+        echo "Already exist : ${dir}"
     fi
-    exit 0
+done
+
+local ofile file
+# Make symbolic link
+echo $'\n# Make symbolic link'
+for ofile in "${OFILES[@]}"; do
+    file=${ofile//\/.dotfiles/}
+    if [ ! -e "${file}" ]; then
+        echo "${ofile} --> ${file}"
+        ln -s ${ofile} ${file}
+    elif [ ! -f "${file}" ]; then
+        echo "Cannot create : ${file}"
+    else
+        echo "Already exist : ${file}"
+    fi
+done
+
+
+# ---------------- other config ---------------- #
+
+# Make symbolic link of .zshenv into ~/
+if [ -f ~/.zsh/.zshenv -a ! -f ~/.zshenv ]; then
+    echo "~/.zsh/.zshenv --> ~/.zshenv"
+    ln -sf ~/.zsh/.zshenv ~/.zshenv
 fi
-install
+
+echo $'\n\n|++++++++++++++++ DOTFILES IS INSTALLED ! ++++++++++++++++|\n\n'
 
 
 # vim: foldmethod=marker
