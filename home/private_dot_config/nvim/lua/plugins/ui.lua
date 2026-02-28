b817eacf67c2fee0a37740516b73e68df9880ffc
K-Gamma <ganma.kato@gmail.com>
2026-03-01 05:12:26 +0900

    fix(nvim): gitsigns の next_hunk/prev_hunk を nav_hunk に移行

--- home/private_dot_config/nvim/lua/plugins/ui.lua
+++ home/private_dot_config/nvim/lua/plugins/ui.lua
@@ -5,34 +5,26 @@ return {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add          = { text = '+' },
-        change       = { text = '~' },
-        delete       = { text = '_' },
-        topdelete    = { text = '‾' },
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
         local gs = require 'gitsigns'
-        local map = function(mode, keys, func, desc)
-          vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = 'Git: ' .. desc })
-        end
+        local map = function(mode, keys, func, desc) vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = 'Git: ' .. desc }) end
 
-        -- hunk navigation（diff モードとの兼ね合いあり）
-        map('n', ']h', function()
-          if vim.wo.diff then vim.cmd.normal { ']c', bang = true }
-          else gs.next_hunk() end
-        end, 'Next Hunk')
-        map('n', '[h', function()
-          if vim.wo.diff then vim.cmd.normal { '[c', bang = true }
-          else gs.prev_hunk() end
-        end, 'Prev Hunk')
+        -- hunk navigation
+        map('n', ']h', function() gs.nav_hunk('next') end, 'Next Hunk')
+        map('n', '[h', function() gs.nav_hunk('prev') end, 'Prev Hunk')
 
         -- hunk 操作
-        map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk,       'Stage Hunk')
-        map({ 'n', 'v' }, '<leader>hr', gs.reset_hunk,       'Reset Hunk')
-        map('n',          '<leader>hu', gs.undo_stage_hunk,   'Undo Stage Hunk')
-        map('n',          '<leader>hp', gs.preview_hunk,      'Preview Hunk')
-        map('n',          '<leader>hb', function() gs.blame_line { full = true } end, 'Blame Line')
+        map({ 'n', 'v' }, '<leader>hs', gs.stage_hunk, 'Stage Hunk')
+        map({ 'n', 'v' }, '<leader>hr', gs.reset_hunk, 'Reset Hunk')
+        map('n', '<leader>hu', gs.undo_stage_hunk, 'Undo Stage Hunk')
+        map('n', '<leader>hp', gs.preview_hunk, 'Preview Hunk')
+        map('n', '<leader>hb', function() gs.blame_line { full = true } end, 'Blame Line')
       end,
     },
   },
@@ -47,7 +39,7 @@ return {
         { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-        { 'gr',        group = 'LSP Actions', mode = { 'n' } },
+        { 'gr', group = 'LSP Actions', mode = { 'n' } },
       },
     },
   },
